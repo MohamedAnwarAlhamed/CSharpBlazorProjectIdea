e37384e606c61537522a04b24fd7f14544cc4a1f
Mohammed Anwar <Alhamed202024@gmail.com>
2024-10-06 22:48:24 +0300

    Seperated File page about File code

---
+++ NewsTracker/Components/Pages/DetalisNew.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+using NewsTracker.Model;
+using NewsTracker.Services;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace NewsTracker.Components.Pages
+{
+    public partial class  DetalisNew
+    {
+
+        [Parameter]
+        public int newsId { get; set; }
+
+        [Inject] private NewsService NewsService { get; set; }
+
+        private NewsItem newsItem = new NewsItem();
+
+        [Inject] private NavigationManager NavigationManager { get; set; }
+        [Inject] private IJSRuntime JSRuntime { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            newsItem = await NewsService.GetNewsByIdAsync2(newsId); // جلب تفاصيل الخبر
+        }
+    }
+}
+
+
+
+
